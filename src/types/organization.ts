735a0165abd766a859594fd40709f995f27b4637
Saad Makrod <saad.makrod@gmail.com>
2023-12-25 11:44:37 -0500

    defining user contract

--- src/types/organization.ts
+++ src/types/organization.ts
@@ -1,32 +1,32 @@
+import { Id } from "./propertyTypes"
+import User from "./user"
+
+export type CompanyFunding = 'EF' | 'PS' | 'SR' | 'SA' | 'SB' | 'NF'
+
+export type CompanyStage = 'ID' | 'DV' | 'PR' | 'PO'
+
+export type CompanyChallenges = 'SF' | 'PD' | 'BC' | 'BS' | 'MB' | 'SO' | 'MF' | 'LR' | 'HT'
+
 type Organization = {
-    'id': 2,
-        'user_set': [
-            {
-                'first_name': 'Saad',
-                'last_name': 'Makrod',
-                'email': 'saad.makrod@mail.utoronto.ca',
-                'linkedin': null,
-                'bio': null,
-                'photo': null,
-                'role': 'SWE',
-                'country': null
-            }
-        ],
-        'updated': '2023-09-23T19:58:08.176718Z',
-        'created': '2023-09-23T19:47:04.062015Z',
-        'name': 'Test Company',
-        'website': 'https://www.linkedin.com/in/saad-makrod/',
-        'logo': null,
-        'identifier': 'test-company',
-        'industry': 'Pharmaceutical',
-        'linkedin': null,
-        'twitter': null,
-        'facebook': null,
-        'tagline': null,
-        'stage': 'ID',
-        'funding': 'NF',
-        'challenge1': 'MB',
-        'challenge2': 'LR',
-        'challenge3': 'HT',
-        'owner': 1
+    id: Id,
+    user_set: [User],
+    updated: string,
+    created: string,
+    name: string,
+    website: string | null,
+    logo: string | null,
+    identifier: string,
+    industry: string | null,
+    linkedin: string | null,
+    twitter: string | null,
+    facebook: string | null,
+    tagline: string | null,
+    stage: CompanyStage,
+    funding: CompanyFunding,
+    challenge1: CompanyChallenges,
+    challenge2: CompanyChallenges,
+    challenge3: CompanyChallenges,
+    owner: Id
 }
+
+export default Organization
