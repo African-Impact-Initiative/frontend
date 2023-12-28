5773a8ef0da7c1d427377158bef98014713f7682
Saad Makrod <saad.makrod@gmail.com>
2023-12-28 12:36:32 -0500

    adding temp routes

---
+++ src/routes/pathConstants.ts
@@ -0,0 +1,18 @@
+const PathConstants: { [key: string]: string } = {
+    home: '/',
+    about: '/about/',
+    login: '/login/',
+    signUp: '/register/',
+    signUpVerification: '/register/verification',
+    forgotPassword: '/forgot-password/',
+    confirmForgotPassword: '/confirm-forgot-password/',
+    terms: '/onboarding/terms-of-use',
+    personal: '/onboarding/personal-information',
+    onboarding: '/onboarding/path',
+    companyProfile: '/onboarding/company-profile',
+    developmentStage: '/development/stage',
+    developmentChallenges: '/development/challenges',
+    developmentFundraising: '/development/fundraising',
+}
+
+export default PathConstants
