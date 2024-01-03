fbcc950c307effe18adfe95c38ceabbeb7665f99
Saad Makrod <saad.makrod@gmail.com>
2024-01-03 10:38:07 -0500

    adding components

--- src/components/VBAccordion.tsx
+++ src/components/VBAccordion.tsx
@@ -12,26 +12,28 @@ interface AccordianProps {
 }
 
 const VBAccordion = ({ list, key }: AccordianProps) => {
-    return <Container>
-        {
-            list.map((value, i) =>
-                <Accordion key={`accordion-${key}-value-${i}`} sx={{boxShadow: 'none', marginBottom: '10px'}}>
-                    <AccordionSummary
-                        expandIcon={<AddCircleOutlineOutlinedIcon />}
-                        aria-controls={`panel${key}${i}-content`}
-                        id={`panel${key}${i}-header`}
-                    >
-                        <Typography>{value.title}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography>
-                            {value.description}
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
-            )
-        }
-    </Container>
+    return (
+        <Container maxWidth='md'>
+            {
+                list.map((value, i) =>
+                    <Accordion key={`accordion-${key}-value-${i}`} sx={{boxShadow: 'none', marginBottom: '10px'}}>
+                        <AccordionSummary
+                            expandIcon={<AddCircleOutlineOutlinedIcon />}
+                            aria-controls={`panel${key}${i}-content`}
+                            id={`panel${key}${i}-header`}
+                        >
+                            <Typography>{value.title}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                {value.description}
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                )
+            }
+        </Container>
+    )
 }
 
 export default VBAccordion
---
+++ src/components/VBLogo.tsx
@@ -0,0 +1,20 @@
+import { Box, Icon, Typography } from "@mui/material"
+import { Link } from "react-router-dom"
+import PathConstants from "../routes/pathConstants"
+
+const VBLogo = () => {
+    return (
+        <Link to={PathConstants.home} style={{textDecoration: 'none'}}>
+            <Box sx={{  display: 'flex', alignItems: 'center', marginRight: '20px'}}>
+                <Icon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1}}>
+                    <img src={ventureIcon} alt="logo" />
+                </Icon>
+                <Box>
+                    <Typography variant="p" sx={{ color: '#101828', fontWeight: '600', fontSize: '18px', lineHeight: '28px',  }}>Venture Build</Typography>
+                </Box>
+            </Box>
+        </Link>
+    )
+}
+
+export default VBLogo
